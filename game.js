94140fb3430a1ef4a29c5b3cf8e12d42eacad269
Garnet638 <garnet638@gmail.com>
2017-09-21 20:26:48 -0400

    Got GUI working, added prettification

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 function tab(tabName){
   $('.gameTab').slideUp(0);
+  $('.gameTabSelector').removeClass('selectedTab');
+  $('#tab'+tabName).addClass('selectedTab');
   $('#'+tabName).slideDown(0);
 }
